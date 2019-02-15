065494aa053b19fa156c878f6d444bb59af37380
Antonio Vivace <avivace4@gmail.com>
2019-02-15 01:08:55 +0100

    Inverse DCT2

--- dct.py
+++ dct.py
@@ -1,5 +1,5 @@
 from numpy import array, empty
-from scipy.fftpack import fft, dct
+from scipy.fftpack import fft, dct, idct
 import random
 import time
 
@@ -26,22 +26,33 @@ for i in range(0, len(a)):
 sample = empty([8,8])
 
 sample = array([[231, 32, 233, 161, 24, 71, 140, 245],
-[247, 40, 248, 245, 124, 204, 36, 107],
-[234 ,202, 245, 167, 9, 217, 239, 173],
-[193, 190, 100, 167, 43, 180, 8, 70],
-[11, 24, 210, 177, 81, 243, 8, 112],
-[97, 195, 203, 47, 125, 114, 165, 181],
-[193, 70, 174, 167, 41, 30, 127, 245],
-[87, 149, 57, 192, 65, 129, 178, 228]])
+				[247, 40, 248, 245, 124, 204, 36, 107],
+				[234 ,202, 245, 167, 9, 217, 239, 173],
+				[193, 190, 100, 167, 43, 180, 8, 70],
+				[11, 24, 210, 177, 81, 243, 8, 112],
+				[97, 195, 203, 47, 125, 114, 165, 181],
+				[193, 70, 174, 167, 41, 30, 127, 245],
+				[87, 149, 57, 192, 65, 129, 178, 228]])
 
 print(sample)
 
 print(dct(sample[0], 2, norm = 'ortho'))
 
+# DCT2 (DCT by row and then by column)
 for i in range(0,8):
 	sample[i] = dct(sample[i], 2, norm = 'ortho')
 
 for i in range(0,8):
 	sample[:, i] = dct(sample[:, i], 2, norm = 'ortho')
+
+print(sample)
+
+# Inverse DCT2
+
+for i in range(0,8):
+	sample[:, i] = idct(sample[:, i], 2, norm = 'ortho')
+
+for i in range(0,8):
+	sample[i] = idct(sample[i], 2, norm = 'ortho')
 
 print(sample)
