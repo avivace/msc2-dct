b0ea315093875446dcf5a4d0a645ca6cb6c5e075
Antonio Vivace <avivace4@gmail.com>
2019-02-15 14:12:31 +0100

    VueJS frontend UI

---
+++ vueapp/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <dct />
+  </div>
+</template>
+
+<script>
+import dct from './components/dct.vue'
+
+export default {
+  name: 'app',
+  components: {
+    dct
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vueapp/src/components/dct.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="hello">
+		<h1>MCS2 - JPEG UI </h1>
+		<h2> Antonio Vivace - February 2019 </h2>
+		
+		<form enctype="multipart/form-data">
+		<input type="file" name="sourceImage" @change="filesChange($event.target.files);"
+						accept="image/*" class="input-file">
+				</form>
+		<img :src=startingImage width=500 height=500 v-if="startingImage">
+
+	</div>
+</template>
+
+<script>
+export default {
+	data: function(){ return {
+		startingImage: null,
+	}},
+	methods:{
+		filesChange: function(file){
+			var reader = new FileReader();
+
+			// Render the selected image
+			reader.onload = function(e){
+				console.log(e.target.result)
+				this.startingImage = e.target.result;
+			}.bind(this)
+
+			reader.readAsDataURL(file[0]);
+
+			// Upload it to the backend
+			var formData = new FormData();
+			formData.append("sourceImage", file[0]);
+			this.$axios.post('http://localhost:5000/image', formData, {
+				headers: {
+					'Content-Type': 'multipart/form-data'
+					}
+				})  
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
