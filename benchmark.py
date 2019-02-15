097c356f8fb0cc7c17a3238210e7d2c255898448
Antonio Vivace <avivace4@gmail.com>
2019-02-15 08:46:51 +0100

    Use numpy.random.rand() to fill arrays with random values

--- benchmark.py
+++ benchmark.py
@@ -1,23 +1,29 @@
-from dct import dct2, idct2
+from dct import dct2, idct2, my_dct2
 import random
 import time
-from numpy import empty
+from numpy import empty, allclose, random
+import csv
+
+wtr = csv.writer(open ('times_scipy.csv', 'w'), delimiter=',', lineterminator='\n')
+wtr.writerow(["n","scipyTime", "myTime"])
 
 dummies = []
 
 def createDummy(size):
-	a = empty([size, size])
-	for i in range(size):
-		for j in range(size):
-			a[i,j] = random.randint(0,256)
+	a = random.rand(size,size)
 	return a
 
-for i in range(5,6):
-	print("Generating", i*500, "x",i*500, "array...")
-	dummies.append(createDummy(i*500))
+for i in range(1,10):
+	print("Generating", i*1000, "x", i*1000, "array...")
+	dummies.append(createDummy(i*1000))
 
 for i in range(0, len(dummies)):
 	t = time.time()
-	dct2(dummies[i])
-	elapsed = time.time() - t
-	print("Size:",(i+5)*500,", scipyDCTtime:", elapsed, "s, myDCTtime:","","s")
+	scipy=dct2(dummies[i])
+	elapsedSciPy = time.time() - t
+	#t = time.time()
+	#my=my_dct2(dummies[i])
+	#elapsedMy = time.time() - t
+	#print(allclose (scipy, my))
+	wtr.writerow([dummies[i].shape[0],elapsedSciPy])
+	#print("Size:",dummies[i].shape[0],", scipyDCTtime:", elapsedSciPy, "s, myDCTtime:",elapsedMy,"s")
