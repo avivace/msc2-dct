9a97c7ece2eb13757052f96602c8a7483ca3d5d0
Antonio Vivace <avivace4@gmail.com>
2019-02-15 17:16:07 +0100

    Image upload/response ping pong: idct(dct(arrayFromImage)) and back to the frontend as UTF-8, ready to be converted in base64 and rendered again

--- vueapp/src/components/dct.vue
+++ vueapp/src/components/dct.vue
@@ -7,7 +7,9 @@
 		<input type="file" name="sourceImage" @change="filesChange($event.target.files);"
 						accept="image/*" class="input-file">
 				</form>
+		{{ height }} x {{width }}
 		<img :src=startingImage width=500 height=500 v-if="startingImage">
+		<img :src=outputImage width=500 height=500 v-if="startingImage">
 
 	</div>
 </template>
@@ -16,6 +18,9 @@
 export default {
 	data: function(){ return {
 		startingImage: null,
+		height: null,
+		width: null,
+		outputImage:null,
 	}},
 	methods:{
 		filesChange: function(file){
@@ -25,6 +30,10 @@ export default {
 			reader.onload = function(e){
 				console.log(e.target.result)
 				this.startingImage = e.target.result;
+				let image = new Image();
+				image.src = e.target.result;
+				this.height = image.height;
+				this.width = image.width;
 			}.bind(this)
 
 			reader.readAsDataURL(file[0]);
@@ -32,13 +41,15 @@ export default {
 			// Upload it to the backend
 			var formData = new FormData();
 			formData.append("sourceImage", file[0]);
+			var self = this;
 			this.$axios.post('http://localhost:5000/image', formData, {
 				headers: {
 					'Content-Type': 'multipart/form-data'
 					}
 				})  
 			.then(function (response) {
-				console.log(response);
+				console.log(response.data)
+				self.outputImage = response.data.image
 			})
 			.catch(function (error) {
 				console.log(error);
--- webserver.py
+++ webserver.py
@@ -1,15 +1,44 @@
 from dct import dct2, idct2
-from flask import Flask, request
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import io 
+import numpy as np
+import base64
 
 app = Flask(__name__)
 CORS(app)
 
+def serve_pil_image(pil_img):
+    img_io = io.BytesIO()
+    print()
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/bmp')
+
+# Dunno, not sure we actually need to base64 the buffer/PIL image
+# We still need to serialize it to wrap it in JSON, someway
+
+def pil_to_base64(pil_img):
+    img_io = io.BytesIO()
+    pil_img.save(img_io, format='bmp')
+    return base64.b64encode(img_io.getvalue())
+
 @app.route('/image', methods=["POST"])
 def image():
-    sourceImage = request.files["sourceImage"].read()
-    #file = request.files['sourceImage']
-    #print(file)
-    return 'Hello, World!'
+    bytestream = request.files["sourceImage"].read()
+    img = Image.open(io.BytesIO(bytestream))
+    imagearray = np.asarray( img, dtype="float" )
+    print(imagearray.shape)
+    h = imagearray.shape[0]
+    w = imagearray.shape[1]
+    print(dct2(imagearray))
+
+    # DO THINGS ON THE IMAGE
+    img = Image.fromarray(idct2(dct2(imagearray)).astype(np.uint8), mode='L')
+
+    # Back to UTF8, which is JSON safe, we will base64 it again on the frontend..
+    return jsonify(image="data:image/bmp;base64,"+pil_to_base64(img).decode('utf8'))
 
 app.run()
