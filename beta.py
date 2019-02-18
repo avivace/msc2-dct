491dcbfc83c00b0436c96984095097bfcc182639
Antonio Vivace <avivace4@gmail.com>
2019-02-18 01:01:31 +0100

    Use numpy clip for normalization

--- beta.py
+++ beta.py
@@ -1,3 +1,4 @@
+from numpy import clip
 def apply_beta_transform(d, beta, imagearray):
     """
     moltiplicare per il coefficiente β solo le frequenze c_kl con k + l ≥ d (sto assumendo
@@ -15,16 +16,7 @@ def apply_beta_transform(d, beta, imagearray):
 
 def norm(f):
     """
-    arrotondare ff all’intero pi`u vicino, mettere a zero i valori negativi e a 255 quelli
+    arrotondare ff all’intero più vicino, mettere a zero i valori negativi e a 255 quelli
     maggiori di 255 in modo da avere una immagine ammissibile
     """
-    h = f.shape[0]
-    w = f.shape[1]
-    for k in range(0,h):
-        for l in range(0,w):
-            if f[k,l] < 0:
-                f[k,l] = 0
-            elif f[k,l] > 255:
-                f[k,l] = 255
-            else:
-                f[k,l] = int(round(f[k,l]))
+    clip(f, 0, 255)
