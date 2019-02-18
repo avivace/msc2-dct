70f21963cd0a730d3e6f955f17533e73b203b5de
Antonio Vivace <avivace4@gmail.com>
2019-02-18 01:01:07 +0100

    Import ApexCharts

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -4,6 +4,11 @@ import axios from 'axios'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
+
+Vue.component('apexchart', VueApexCharts)
+
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
